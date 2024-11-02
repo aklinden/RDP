1ab732907ea479c768fc68b4575ff329dc9c0eaf
Andy Linden <aklinden0@gmail.com>
2024-11-02 12:16:42 -0700

    Made edit to the param section

--- RDPFixV1.ps1
+++ RDPFixV1.ps1
@@ -1,13 +1,12 @@
 function Restart-RDP {
     [cmdletbinding()]
     param (
-        OptionalParameters
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string] $ComputerName
     )
-    
-}
-
-$rdpTcp = qwinsta | Select-String "rdp-tcp"
+    $rdpTcp = qwinsta | Select-String "rdp-tcp"
 if ($rdpTcp -eq $null) {
 
     Get-Service -Name TermService | Restart-Service -Force
+}
 }
