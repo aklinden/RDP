83b3f3167d990c3e348f474078ad666767958505
Andy Linden <aklinden0@gmail.com>
2024-11-02 12:34:12 -0700

    Ran an autoformat to clean up code, and fixed a $null comparison. Added Invoke-command to work with the $computername parameter. Added an else statement to inform the user that the issues was not detected.

--- RDPFixV1.ps1
+++ RDPFixV1.ps1
@@ -1,12 +1,19 @@
 function Restart-RDP {
     [cmdletbinding()]
     param (
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [string] $ComputerName
     )
-    $rdpTcp = qwinsta | Select-String "rdp-tcp"
-if ($rdpTcp -eq $null) {
 
-    Get-Service -Name TermService | Restart-Service -Force
-}
+    Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+        $rdpTcp = qwinsta | Select-String "rdp-tcp"
+        if ($null -eq $rdpTcp) {
+            Write-Warning -Message "No RDP client detected, restarting RDP service..."
+            Get-Service -Name TermService | Restart-Service -Force
+        }
+        else{
+            Write-Host "RDP client found, no changes needed."
+        }
+    }
+    
 }
